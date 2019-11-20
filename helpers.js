e9e47848fe5f24613ba1277e7b8420d4a331c13e
eduardo contreras <eduardo.contreras@wizeline.com>
2019-11-20 17:31:24 -0600

    Refactor multiply adding const before return

--- helpers.js
+++ helpers.js
@@ -7,7 +7,8 @@ const divide = (number1, number2) => {
 }
 
 const multiply = (number1, number2) => {
-  return number1 * number2;
+  const result = number2 * number1
+  return result;
 }
 
 const substract = (number1, number2) => {
