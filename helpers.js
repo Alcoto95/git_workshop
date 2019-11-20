22289534e005b1e5fd0e92652c8c6c156995da76
Alberto Copado Torruco <alberto.copado@wizeline.com>
2019-11-20 17:34:26 -0600

    fix multiplication function

--- helpers.js
+++ helpers.js
@@ -7,7 +7,8 @@ const divide = (number1, number2) => {
 }
 
 const multiply = (number1, number2) => {
-  return number1 * number2;
+  const result = number1 * number2;
+  return result;
 }
 
 const substract = (number1, number2) => {
